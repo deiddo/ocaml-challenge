fbfa4cdf94e2f819dd868b4669b7533b32e2270b
deiddo <aledei2004@outlook.it>
2024-10-15 18:34:58 +0200

    .

---
+++ 1/xor/xor.ml
@@ -0,0 +1,9 @@
+let xor1 a b =
+  ((not a) && b) || (a && (not b));;
+
+
+let xor2 a b = match(a, b) with
+  (false,false) -> false
+  |(false,true) -> true
+  |(true,false) -> true
+  |(true,true) -> false;;

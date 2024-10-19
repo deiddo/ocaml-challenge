8f907fa14159a2384919ebb7bdf65966769f9fea
deiddo <aledei2004@outlook.it>
2024-10-19 19:00:15 +0200

    .

--- 1/nand/nand.ml
+++ 1/nand/nand.ml
@@ -2,7 +2,7 @@ let nand1 a b =
   not (a && b);;
 
 let nand2 a b = match(a,b) with
-  (false,false) -> true
+  |(false,false) -> true
   |(false,true) -> true
   |(true,false) -> true
   |(true,true) -> false
